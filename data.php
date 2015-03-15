f09eb36d6a2cb056e21579f9dc6307eb7fed4cad
emgullufsen <ericgullufsen@gmail.com>
2015-03-15 15:33:18 -0800

    ooooo fug

--- data.php
+++ data.php
@@ -1,3 +1,7 @@
+<?php
+include 'info.php';
+$un = $_REQUEST['username'];
+echo <<<HTM
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -6,10 +10,24 @@
 <title>data</title>
 </head>
 <body>
-<?php
-	$un = $_REQUEST['username'];
-	echo "<h1>You Are logged in $un </h1>";
-?>
-	<div id="favplayers"></div>
+<h1>You Are logged in $un </h1>
+<div id="favplayers">
+<table>
+HTM;
+
+$uid = $_REQUEST['uid'];
+$m = new mysqli($lh, $un, $pass, $db);
+$stmt = $m->prepare("SELECT `name` FROM teams t INNER JOIN usersteams ut ON t.`id` = ut.tid WHERE ut.uid = ?");
+$stmt->bind_param("i", $uid);
+$stmt->execute();
+$stmt->bind_result($llamo);
+while($stmt->fetch()){
+	echo "<tr><td>" . $llamo . "</td></tr>";
+}
+echo <<<HTM
+</table>
+</div>
 </body>
 </html>
+HTM;
+?>
---
+++ info.php
@@ -0,0 +1,8 @@
+<?php
+
+$lh = 'oniddb.cws.oregonstate.edu';
+$un = 'gullufse-db';
+$pass = 'eXVI6J83NBdQ5EiB';
+$db = 'gullufse-db';
+
+?>
